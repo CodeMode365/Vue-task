ee178fd0c0fd8c22b81d04a36e424e69ba269ef1
Pabin234 <codemode365@gmail.com>
2024-04-20 17:13:49 +0545

    hero section

---
+++ src/components/Home/Hero.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="text-md body-font">
+    <div class="container mx-auto flex px-5 py-12 items-center justify-center flex-col">
+      <img
+        class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
+        alt="hero"
+        src="https://dummyimage.com/720x600"
+      />
+      <div class="text-center w-1/3">
+        <h1 class="title-font sm:text-4xl text-3xl font-medium">Shop the latest trends</h1>
+        <p class="leading-relaxed mb-4">
+          Discover the latest fashion trends and explore our curated collection of clothing,
+          accessories, and more. Shop now and stay ahead of the fashion curve.
+        </p>
+        <div class="flex justify-center mt-4">
+          <button
+            class="inline-flex text-white bg-secondary border-0 py-2 px-6 focus:outline-none hover:bg-secondary rounded text-lg hover:bg-secondary/80"
+          >
+            Explore
+          </button>
+          <button
+            class="ml-4 inline-flex text-primary border border-primary bg-white hover:bg-white/80 py-2 px-6 focus:outline-none rounded text-lg"
+          >
+            Contact
+          </button>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
