a9fb9c0cee79f4972b853046614240a2ccfd04b0
Pabin234 <codemode365@gmail.com>
2024-04-20 23:53:13 +0545

    updated hero image

--- src/components/Home/Hero.vue
+++ src/components/Home/Hero.vue
@@ -1,11 +1,11 @@
 <template>
-  <section class="text-md body-font">
+  <section class="text-md body-font" id="main">
     <div class="container mx-auto flex px-5 py-12 items-center justify-center flex-col">
-      <img
-        class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
-        alt="hero"
-        src="https://dummyimage.com/720x600"
-      />
+      <div
+        class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded border-b shadow-primary hover:shadow-xl transition-all cursor-pointer border shadow-lg"
+      >
+        <img class="" alt="hero" src="/src//assets//store.jpg" />
+      </div>
       <div class="text-center w-1/3">
         <h1 class="title-font sm:text-4xl text-3xl font-medium">Shop the latest trends</h1>
         <p class="leading-relaxed mb-4">
@@ -14,7 +14,7 @@
         </p>
         <div class="flex justify-center mt-4">
           <button
-            class="inline-flex text-white bg-secondary border-0 py-2 px-6 focus:outline-none hover:bg-secondary rounded text-lg hover:bg-secondary/80"
+            class="inline-flex text-white bg-secondary border-0 py-2 px-4 focus:outline-none hover:bg-secondary rounded text-lg hover:bg-secondary/80"
           >
             Explore
           </button>
