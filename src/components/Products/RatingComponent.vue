3e35c504fa64b2445828afc45e782b7ea3129c18
Pabin234 <codemode365@gmail.com>
2024-04-20 23:52:49 +0545

    product rating component

---
+++ src/components/Products/RatingComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <span v-for="(star, i) in Math.floor(props.rating.rate)" :key="'filled-star-' + i"
+      ><i class="fa-solid fa-star text-yellow-500 mx-[1px]"></i
+    ></span>
+
+    <span v-for="(star, i) in 5 - Math.floor(props.rating.rate)" :key="'filled-star-' + i"
+      ><i class="fa-regular fa-star text-yellow-500 mx-[1px]"></i
+    ></span>
+
+    <span class="text-sm" v-if="withCount"> ({{ props.rating.count }})</span>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+const props = defineProps({
+  rating: {
+    rate: Number,
+    count: Number
+  },
+  withCount: Boolean
+})
+</script>
