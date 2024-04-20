41b579efd06dfe37918612f6e3f6bfdf5ebb42ec
Pabin234 <codemode365@gmail.com>
2024-04-20 23:54:24 +0545

    updated navlinks

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,29 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView, RouterLink } from 'vue-router'
+import Navbar from './components/Navbar.vue'
+import { useApiStore } from './stores/product'
+import { storeToRefs } from 'pinia'
+const store = useApiStore()
+const { cart } = storeToRefs(store)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="min-h-screen bg-white text-gray-">
+    <div>
+      <Navbar>
+        <RouterLink class="mx-2" to="/">Home</RouterLink>
+        <RouterLink class="mx-2" to="/products">Products</RouterLink>
+        <RouterLink class="mx-2 relative" to="/products/123"
+          >Cart
+          <span
+            class="absolute -top-1/3 -right-1/2 border rounded-full h-5 w-5 flex items-center justify-center text-white font-semibold bg-secondary border-primary"
+            >{{ cart.length }}</span
+          >
+        </RouterLink>
+      </Navbar>
+      <main class="container mx-auto p-4">
+        <RouterView />
+      </main>
     </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
