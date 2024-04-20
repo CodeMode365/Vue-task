201dd9c632a1ad2fb5038d8e6d0f7b97968856a6
Pabin234 <codemode365@gmail.com>
2024-04-20 23:53:47 +0545

    updated width

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -5,13 +5,11 @@ import ProductListing from '@/components/Products/ProductListing.vue'
 
 <template>
   <div class="py-12 grid grid-cols-12 gap-4">
-    <div class="border rounded-sm col-span-3">
+    <!-- <div class="border rounded-sm col-span-12 md:col-span-3">
       <FilterBar />
-    </div>
-    <div class="col-span-9">
+    </div> -->
+    <div class="col-span-12 md:col-span-12">
       <ProductListing />
     </div>
   </div>
 </template>
-
-<style scoped></style>
