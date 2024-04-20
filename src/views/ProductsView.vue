834b3ed735fad7f6fa2362f5e760a1403ac6729d
Pabin234 <codemode365@gmail.com>
2024-04-20 17:12:49 +0545

    product layout with api

---
+++ src/views/ProductsView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import FilterBar from '@/components/Products/FilterBar.vue'
+import ProductListing from '@/components/Products/ProductListing.vue'
+</script>
+
+<template>
+  <div class="py-12 grid grid-cols-12 gap-4">
+    <div class="border rounded-sm col-span-3">
+      <FilterBar />
+    </div>
+    <div class="col-span-9">
+      <ProductListing />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
