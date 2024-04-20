5e99fbe8d9b11d7ac23d4a8baf4abc964c17b551
Pabin234 <codemode365@gmail.com>
2024-04-20 08:34:42 +0545

    init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <br />
+    <!-- <div class="flex flex-row">
+      <button @click="doubleCount">double</button>
+      <button @click="increment">increment</button>
+      <button @click="inc">inc</button>
+
+      <h2>{{ count }}</h2>
+      <button @click="dcr">Dec</button>
+    </div>
+  </div> -->
+</template>
+
+<script setup>
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
