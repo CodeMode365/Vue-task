12f0e6a7538b18e0ce87c9d0e43ab12b4e6df2f3
Pabin234 <codemode365@gmail.com>
2024-04-21 14:17:56 +0545

    cart navigation

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterView, RouterLink } from 'vue-router'
 import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
 import { useApiStore } from './stores/product'
 import { storeToRefs } from 'pinia'
 const store = useApiStore()
@@ -13,7 +14,7 @@ const { cart } = storeToRefs(store)
       <Navbar>
         <RouterLink class="mx-2" to="/">Home</RouterLink>
         <RouterLink class="mx-2" to="/products">Products</RouterLink>
-        <RouterLink class="mx-2 relative" to="/products/123"
+        <RouterLink class="mx-2 relative" to="/cart"
           >Cart
           <span
             class="absolute -top-1/3 -right-1/2 border rounded-full h-5 w-5 flex items-center justify-center text-white font-semibold bg-secondary border-primary"
@@ -24,6 +25,7 @@ const { cart } = storeToRefs(store)
       <main class="container mx-auto p-4">
         <RouterView />
       </main>
+      <Footer />
     </div>
   </div>
 </template>
