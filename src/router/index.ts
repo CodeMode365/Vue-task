6caf766cc62fdb00a7b4d36e1d912f2f776a9042
Pabin234 <codemode365@gmail.com>
2024-04-21 09:17:59 +0545

    create routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
+import ProductsView from '@/views/ProductsView.vue'
+import ProductDetailView from '@/views/ProductDetailView.vue'
+import NotFound from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +13,19 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/products',
+      name: 'products',
+      component: ProductsView
+    },
+    {
+      path: '/products/:id',
+      name: 'product-detail',
+      component: ProductDetailView
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'NotFound',
+      component: NotFound
     }
   ]
 })
