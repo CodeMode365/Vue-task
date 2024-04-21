09ca2013578345119c11f6aa49d746286d6599e4
Pabin234 <codemode365@gmail.com>
2024-04-21 12:05:42 +0545

    added toaster

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import './assets/main.css'
+import 'vue-toastification/dist/index.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Toast from 'vue-toastification'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+app.use(Toast)
 
 app.use(createPinia())
 app.use(router)
