0ff4f0e10ad135c935b7dac6c23c714ed4a721c6
Pabin234 <codemode365@gmail.com>
2024-04-22 14:10:29 +0545

    footer in cart fixed

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="text-gray-600 body-font">
+  <footer class="text-gray-600 body-font w-full">
     <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
       <a
         class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900"
